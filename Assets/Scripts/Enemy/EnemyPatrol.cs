fdb38d6b9cfba7c359f44065cee67d22b9eb7109
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2021-01-20 19:14:21 -0800

    Update animations

---
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,59 @@
+﻿/**
+Set static points; have mesagent move between these 2 points in a linear line;
+ feature to back back to a previous point
+*/
+using UnityEngine;
+using UnityEngine.AI; 
+
+
+public class EnemyPatrol : MonoBehaviour
+{
+    // waiting variables
+    [SerializeField] // private variables to show in Unity editor
+    private bool patrolWaiting;
+
+    [SerializeField]
+    private float totalWaitTime;
+
+    [SerializeField] 
+    private List<Waypoint> patrolPoints; 
+
+
+    // base behaviors. no other scripts should need reference to 
+    private List<Waypoint> patrolPoints; 
+    private NavMeshAgent agent;
+    private int currentPatrol;
+    private bool traveling;
+    private bool waiting; 
+    private bool patrolForward;
+    private float waitTimer;
+
+    public Transform moveSpot;
+    private int randomSpot; 
+
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        agent = GetComponent<NashMeshAgent>();
+
+        if (navMeshAgent == null) 
+        { 
+            // nav agent wouldn't be attached to game object
+        }
+        
+        else
+        {
+            if ()
+
+        }
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0,moveSpots.Length);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[tandomSpot].position, speed*Time.deltaTime)
+    }
+}
---
+++ Assets/Scripts/Enemy/NavMeshAIMovement.cs
@@ -0,0 +1,126 @@
+﻿// NavMeshAIMovement.cs
+/** Requirements: NavMeshComponent: 
+*/
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float turnSpeed = 180.0f;
+    public float gravity = 30.0f;
+    private float yVelocity = 0f;
+
+
+    public NavMeshAgent agent; 
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    // variables from patroling 
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    // Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States 
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackER;
+
+
+    private void Awake()
+    {
+        player = GameObject.Find("PlayerObj").transform;
+        agent = GetComponent<NashMeshAgent>();
+
+    }
+
+
+    private void Update()
+    {
+        // Check for sight and attack range 
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRnage && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRnage) AttackPlayer();
+
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint; 
+
+        //Walk point reached
+        if (distanceToWalkPoint.magnitude < 1f) 
+            walkPointSet = false;
+
+
+    }
+
+    private void SearchWalkPoint()
+    {
+        // Calc random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ); 
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void ChasePlyaer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        // Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            // Attack code/animations - enemy slash attack
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0) Invoke(nameof(DestroyEnemy), .5f); 
+    }
+
+
+    private void OnDrawGizmosSelected()
+    /* Method for visualizing enemy sight and attack
+    */
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+
+}
