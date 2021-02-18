11d3e8f0eb0cb98b06a442d7bfa63bd5be3b694c
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2021-02-17 19:43:03 -0800

    Refactor movement script for clarity

--- Assets/Scripts/Player/BasicMovement.cs
+++ Assets/Scripts/Player/BasicMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class BasicMovement : MonoBehaviour
 {
+    // Movement
     public float speed = 10f;
     public float sprint = 0.4f;
     public float rotationSpeed = 2.0f;
@@ -12,32 +13,56 @@ public class BasicMovement : MonoBehaviour
     public float jumpSpeed = 15.0f;
     public float gravity = 30.0f;
 
+    // Player camera position
     public Transform cameraHead;
     public float maxCameraHeadRotation = 80.0f;
     public float minCameraHeadRotation = -80.0f;
     private float currentCameraHeadRotation = 0;
 
-    private CharacterController controller;
-
+    // Health bar
     public int maxHealth = 3;
     public int currentHealth;
     public HealthBar healthBar;
 
+    // Controller
+    private CharacterController controller;
+
+    // Animator
     public Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
-
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
-
         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        playAnimations();
+        playerCameraViewpoint();
+        jump();
+        
+        // Movement
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        yVelocity -= gravity * Time.deltaTime;
+        controller.Move(transform.TransformDirection(input.normalized * speed * Time.deltaTime + yVelocity * Vector3.up * Time.deltaTime));
+
+        if (Input.GetKey(KeyCode.LeftShift) && StaminaBar.instance.currentStamina > 0.1f)
+        {
+            controller.Move(transform.TransformDirection(input.normalized * speed * sprint * Time.deltaTime + yVelocity * Vector3.up * Time.deltaTime));
+            StaminaBar.instance.UseStamina(0.05f);
+        }
+        else if (Input.GetKey(KeyCode.LeftShift) && StaminaBar.instance.currentStamina <= 0f)
+        {
+            controller.Move(transform.TransformDirection(input.normalized * speed * Time.deltaTime + yVelocity * Vector3.up * Time.deltaTime));
+        }
+    }
+
+    void playAnimations()
     {
         if (Input.GetKey("w"))
         {
@@ -83,32 +108,10 @@ public class BasicMovement : MonoBehaviour
         {
             anim.SetBool("isRolling", false);
         }
+    }
 
-
-
-        Vector2 mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        transform.Rotate(Vector3.up, mouseInput.x * rotationSpeed);
-        currentCameraHeadRotation = Mathf.Clamp(currentCameraHeadRotation + mouseInput.y * rotationSpeed, minCameraHeadRotation, maxCameraHeadRotation);
-
-        cameraHead.localRotation = Quaternion.identity;
-        cameraHead.Rotate(Vector3.left, currentCameraHeadRotation);
-
-        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        yVelocity -= gravity * Time.deltaTime;
-        gameObject.GetComponent<CharacterController>().Move(transform.TransformDirection(input.normalized * speed * Time.deltaTime + yVelocity * Vector3.up * Time.deltaTime));
-
-        // Sprint
-        if (Input.GetKey(KeyCode.LeftShift) && StaminaBar.instance.currentStamina > 0.1f)
-        {
-            gameObject.GetComponent<CharacterController>().Move(transform.TransformDirection(input.normalized * speed * sprint * Time.deltaTime + yVelocity * Vector3.up * Time.deltaTime));
-            StaminaBar.instance.UseStamina(0.05f);
-        }
-        else if (Input.GetKey(KeyCode.LeftShift) && StaminaBar.instance.currentStamina <= 0f)
-        {
-            gameObject.GetComponent<CharacterController>().Move(transform.TransformDirection(input.normalized * speed * Time.deltaTime + yVelocity * Vector3.up * Time.deltaTime));
-        }
-
-
+    void jump()
+    {
         // Prevents the isGrounded bug/error where sometimes you can't jump!
         if (controller.isGrounded)
         {
@@ -125,6 +128,15 @@ public class BasicMovement : MonoBehaviour
             yVelocity -= gravity * Time.deltaTime;
             anim.SetBool("isJumping", false);
         }
+    }
+
+    void playerCameraViewpoint()
+    {
+        Vector2 mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        transform.Rotate(Vector3.up, mouseInput.x * rotationSpeed);
+        currentCameraHeadRotation = Mathf.Clamp(currentCameraHeadRotation + mouseInput.y * rotationSpeed, minCameraHeadRotation, maxCameraHeadRotation);
+        cameraHead.localRotation = Quaternion.identity;
+        cameraHead.Rotate(Vector3.left, currentCameraHeadRotation);
     }
 
     public void TakeDamage(int damage)
@@ -153,6 +165,9 @@ public class BasicMovement : MonoBehaviour
 
     public void DeathScreen()
     {
-        SceneManager.LoadScene(4);
+        // Remember to update this number as more scenes are created
+        SceneManager.LoadScene(6);
     }
+
+
 }
