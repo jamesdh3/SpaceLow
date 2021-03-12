a726a96fc1443fc53c9c521f292568423c717aaa
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2021-03-11 20:38:17 -0800

    Add moon to last scene and custom font

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -23,3 +23,8 @@ public class EnemyHealth : MonoBehaviour
 }
 
 // This needs a character controller to work.  Enemies will not take damage without one.
+// Make the character controller circle BIGGER than the box collider.  The CC will by the one that connects the damage function
+// the box collider will be for not running through the enemy.
+
+// CC Collides against objects, ONLY in the direction it's currently moving. So if you have a moving enemy it needs a box collider too,
+// but make sure box collider is smaller than the CC.
